48e1b9c060800b4e7423bea62769532d77aef31d
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2023-03-14 21:16:48 +0300

    переменные в Java Часть 1 Объявление переменной типа "byte" и ее вывод

--- src/com/javalesson/hello/HelloWorld.java
+++ src/com/javalesson/hello/HelloWorld.java
@@ -4,5 +4,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+
+
+        byte b = 10;
+        System.out.println("b = "+b);
+
     }
 }
