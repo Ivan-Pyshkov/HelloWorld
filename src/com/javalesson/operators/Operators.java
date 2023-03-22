b2b1a3a124b5921f195f7442293c3498751e3f6b
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2023-03-22 20:22:03 +0300

    Операторы языка Java. Часть 2 Операция сравнения

--- src/com/javalesson/operators/Operators.java
+++ src/com/javalesson/operators/Operators.java
@@ -13,22 +13,22 @@ public class Operators {
         double f = a / b;
         double g = a % b;
 
-        System.out.println("c = " + c);
+/*        System.out.println("c = " + c);
         System.out.println("d = " + d);
         System.out.println("e = " + e);
         System.out.println("f = " + f);
-        System.out.println("g = " + g);
+        System.out.println("g = " + g);*/
 
 
 //        a = a + 5;
 //        a += 5;
 //        a = a + 1;
 //        a+=1;
-        a++;
+/*        a++;
         System.out.println("New a = " + a);
 
         b--;
-        System.out.println("New b = " + b);
+        System.out.println("New b = " + b);*/
 
 
         double n = 7;
@@ -37,8 +37,19 @@ public class Operators {
         double res1 = 2 * n++;
         double res2 = 2 * ++m;
 
-        System.out.println("res1 = " + res1);
-        System.out.println("res2 = " + res2);
+/*        System.out.println("res1 = " + res1);
+        System.out.println("res2 = " + res2);*/
+
+
+//        == != <> <= >= && || ?:
+
+
+        int x = 3;
+        int y = 5;
+        int z = 8;
+
+        boolean boolVal = x == y;
+        System.out.println("boolVal = " + boolVal);
 
     }
 }
